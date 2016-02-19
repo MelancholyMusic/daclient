d415417ef17d9dad35cd9a37aeec771cdb9bafcb
Michael Fiedel <fiedelmike@gmail.com>
2016-02-18 23:37:42 -0700

    Upgraded to Semantic-UI

--- dndazureClient/app/app.js
+++ dndazureClient/app/app.js
@@ -2,3 +2,22 @@
 
 var app = angular.module('chatModule', []);
 app.value('backendServerUrl', 'http://localhost:54008/');
+
+$(document)
+	.ready(function() {
+
+	// fix main menu to page on passing
+	$('.main.menu').visibility({
+		type: 'fixed'
+	});
+	$('.overlay').visibility({
+		type: 'fixed',
+		offset: 80
+	});
+
+	// show dropdown on hover
+	$('.main.menu  .ui.dropdown').dropdown({
+		on: 'hover'
+	});
+	})
+;
--- dndazureClient/app/controllers.js
+++ dndazureClient/app/controllers.js
@@ -2,17 +2,24 @@
 
 app.controller('ChatController', ['$scope', function($scope, ChatHubProxy)
 	{
-	    var displayName = prompt('Enter your name:', '');
 		console.log('Attempting to connect backendServerUrl=dndazureapi.azurewebsites.net hubName=ChatHub');
 		var connection = $.hubConnection('http://dndazureapi.azurewebsites.net/');
 		var chatHubProxy = connection.createHubProxy('ChatHub');
-
+		var displayName = 'temp';
+		
 		chatHubProxy.on('addNewMessageToPage', function(name, message)
 		{
 			console.log('addNewMessageToPage: name=' + name + ' message=' + message);
-			var encodedName = $('<div />').text(name).html();
-			var encodedMsg = $('<div />').text(message).html();
-			$('#discussion').append('<li><strong>' + encodedName + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+			var date = new Date();
+			var hours = date.getHours();
+			var ampm = 'AM'
+			if(hours > 12)
+			{
+				ampm = 'PM';
+				hours -= 12;
+			}
+
+			$('#discussion').append('<div class="comment"><div class="content"><a class="author">' + name + '</a><div class="metadata"><span class="date">' + hours + ':' + date.getMinutes() + ampm + '</span></div><div class="text">' + message + '</div></div></div>');
 		});
 
 		connection.start()
@@ -21,8 +28,8 @@ app.controller('ChatController', ['$scope', function($scope, ChatHubProxy)
 
 		$('#sendmessage').click(function()
 		{
-			console.log('Calling Send on chatHub name=' + displayName + ' message=' + $('#message').val());
-			chatHubProxy.invoke('Send', displayName, $('#message').val())
+			console.log('Calling Send on chatHub name=' + $('#displayname').val() + ' message=' + $('#message').val());
+			chatHubProxy.invoke('Send', $('#displayname').val(), $('#message').val())
 				.done(function() { console.log('Invocation of Send succeeded'); })
 				.fail(function(error) { console.log('Invocation of Send failed. ' + error) });
 
