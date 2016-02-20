f030054b1b2848696285cbe8d5d3a5b928d3c915
Michael Fiedel <fiedelmike@gmail.com>
2016-02-19 18:20:08 -0700

    added a default page template, and a header/footer that can be used in every page

--- dndazureClient/app/app.js
+++ dndazureClient/app/app.js
@@ -1,23 +1,26 @@
 ﻿'use strict';
 
-var app = angular.module('chatModule', []);
+var app = angular.module('basemodule', []);
 app.value('backendServerUrl', 'http://localhost:54008/');
 
 $(document)
 	.ready(function() {
 
-	// fix main menu to page on passing
-	$('.main.menu').visibility({
-		type: 'fixed'
-	});
-	$('.overlay').visibility({
-		type: 'fixed',
-		offset: 80
-	});
+		// fix main menu to page on passing
+		$('.main.menu').visibility({
+			type: 'fixed'
+		});
+		$('.overlay').visibility({
+			type: 'fixed',
+			offset: 80
+		});
 
-	// show dropdown on hover
-	$('.main.menu  .ui.dropdown').dropdown({
-		on: 'hover'
-	});
+		// show dropdown on hover
+		$('.main.menu  .ui.dropdown').dropdown({
+			on: 'hover'
+		});
+
+		$("#header").load("header.html");
+		$("#footer").load("footer.html");
 	})
 ;
